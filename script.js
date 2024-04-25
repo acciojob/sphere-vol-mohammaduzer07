29b5c90cc985940324d18981be4fd65e267b21fd
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-25 10:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+	let rad = document.getElementById('radius');
+	let volume = document.getElementById('volume');
+
+	let radius = rad.value;
+	let vol = (4/3) * 3.142 * (radius*radius*radius);
+	volume.value = vol;	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
